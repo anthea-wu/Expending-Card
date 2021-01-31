dcac036d817f98220e953594db8b8b2961bd5ad7
litchi <d0509327@mail.fcu.edu.tw>
2021-01-31 18:46:06 +0800

    Show Cards Name in page

--- Expending-Card/Controllers/CatalogCardController.cs
+++ Expending-Card/Controllers/CatalogCardController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Expending_Card.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Expending_Card.Controllers
 {
     public class CatalogCardController : Controller
     {
-        // GET
-        public IActionResult Index()
+        private readonly List<CatalogCards> _catalogCards = new List<CatalogCards>()
         {
+            {new CatalogCards() {CatalogName = "食物", CatalogOrder = 1}},
+            {new CatalogCards() {CatalogName = "飲料", CatalogOrder = 2}},
+            {new CatalogCards() {CatalogName = "交通", CatalogOrder = 3}},
+        };
+
+        // GET
+        public IActionResult Show()
+        {
+            ViewBag.list = _catalogCards;
             return View();
         }
     }
---
+++ Expending-Card/Models/CatalogCards.cs
@@ -0,0 +1,8 @@
+﻿namespace Expending_Card.Models
+{
+    public class CatalogCards
+    {
+        public string CatalogName { get; set; }
+        public int CatalogOrder { get; set; }
+    }
+}
