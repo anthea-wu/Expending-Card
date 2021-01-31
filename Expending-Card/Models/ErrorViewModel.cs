7d5860964e17870f8148605c54a5e825313a358f
litchi <d0509327@mail.fcu.edu.tw>
2021-01-31 16:01:57 +0800

    Create a MVC project

---
+++ Expending-Card/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Expending_Card.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
