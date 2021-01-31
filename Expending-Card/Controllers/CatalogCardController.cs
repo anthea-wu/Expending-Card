d39db75298b914ecc17ac57fe199947e146b9397
litchi <d0509327@mail.fcu.edu.tw>
2021-01-31 19:38:53 +0800

    Show each Cards in each page

--- Expending-Card/Controllers/CatalogCardController.cs
+++ Expending-Card/Controllers/CatalogCardController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Expending_Card.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +10,34 @@ namespace Expending_Card.Controllers
     {
         private readonly List<CatalogCards> _catalogCards = new List<CatalogCards>()
         {
+            {new CatalogCards() {CatalogName = "未分類", CatalogOrder = 0}},
             {new CatalogCards() {CatalogName = "食物", CatalogOrder = 1}},
             {new CatalogCards() {CatalogName = "飲料", CatalogOrder = 2}},
             {new CatalogCards() {CatalogName = "交通", CatalogOrder = 3}},
+            {new CatalogCards() {CatalogName = "美妝", CatalogOrder = 4}},
+            {new CatalogCards() {CatalogName = "學習", CatalogOrder = 5}},
         };
 
         // GET
         public IActionResult Show()
         {
             ViewBag.list = _catalogCards;
+            return View();
+        }
+        
+        [Route("{controller}/show/{name}")]
+        public IActionResult ShowOneCard(string name)
+        {
+            if (_catalogCards.Any(x => x.CatalogName == name))
+            {
+                var order = _catalogCards.FindIndex(x => x.CatalogName == name);
+                ViewBag.card = _catalogCards[order];
+            }
+            else
+            {
+                ViewBag.card = _catalogCards[0];    
+            }
+            
             return View();
         }
     }
