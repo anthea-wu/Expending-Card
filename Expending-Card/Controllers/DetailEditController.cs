f538103e0d0ea2341044c5b8fd29ace8edc3fac7
litchi <d0509327@mail.fcu.edu.tw>
2021-02-02 01:15:13 +0800

    Try to add detail by URL but failed

--- Expending-Card/Controllers/DetailEditController.cs
+++ Expending-Card/Controllers/DetailEditController.cs
@@ -1,12 +1,24 @@
-﻿using System.Collections.Generic;
-using Expending_Card.Models;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Expending_Card.Controllers;
+using Expending_Card.Models;
 
 namespace Expending_Card.Controllers
 {
     public class DetailEditController : Controller
     {
-        private List<ExpendingDetail> _expendingDetails = new List<ExpendingDetail>()
+        private readonly List<CatalogCards> _catalogCards = new List<CatalogCards>()
+        {
+            {new CatalogCards() {CatalogName = "未分類", CatalogOrder = 0}},
+            {new CatalogCards() {CatalogName = "食物", CatalogOrder = 1}},
+            {new CatalogCards() {CatalogName = "飲料", CatalogOrder = 2}},
+            {new CatalogCards() {CatalogName = "交通", CatalogOrder = 3}},
+            {new CatalogCards() {CatalogName = "美妝", CatalogOrder = 4}},
+            {new CatalogCards() {CatalogName = "學習", CatalogOrder = 5}},
+        };
+        
+        private static readonly List<ExpendingDetail> _expendingDetails = new List<ExpendingDetail>()
         {
             new ExpendingDetail()
             {
@@ -23,11 +35,31 @@ namespace Expending_Card.Controllers
                 ExpendingPrice = 12
             }
         };
-            
-        // GET
-        public IActionResult AllDetails()
+        
+        public ActionResult AllDetails()
         {
             ViewBag.list = _expendingDetails;
+            return View();
+        }
+
+        [Route("{card}/{detail}/{price}/{date}")]
+        public IActionResult AddDetails(string card, string detail, int price, string date)
+        {
+            CatalogCards catalogCard = null;
+            var findIndex = _catalogCards.FindIndex(x => x.CatalogName == card);
+            catalogCard = findIndex != -1 ? _catalogCards[findIndex] : _catalogCards[0];
+
+            var newItem = new ExpendingDetail()
+            {
+                ExpendingCard = catalogCard,
+                ExpendingDate = date,
+                ExpendingDetails = detail,
+                ExpendingPrice = price
+            };
+            
+            _expendingDetails.Add(newItem);
+
+            ViewBag.list = newItem;
             return View();
         }
     }
