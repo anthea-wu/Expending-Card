d3746ade1cc79b0c90bac3b307921ef8b83bd8f5
litchi <d0509327@mail.fcu.edu.tw>
2021-02-03 01:10:07 +0800

    Use ViewModel to show CatalogCards

--- Expending-Card/Controllers/CatalogCardController.cs
+++ Expending-Card/Controllers/CatalogCardController.cs
@@ -7,37 +7,41 @@ using Microsoft.AspNetCore.Mvc;
 namespace Expending_Card.Controllers
 { public class CatalogCardController : Controller
     {
-        private readonly List<CatalogCards> _catalogCards = new List<CatalogCards>()
-        {
-            {new CatalogCards() {CatalogName = "未分類", CatalogOrder = 0}},
-            {new CatalogCards() {CatalogName = "食物", CatalogOrder = 1}},
-            {new CatalogCards() {CatalogName = "飲料", CatalogOrder = 2}},
-            {new CatalogCards() {CatalogName = "交通", CatalogOrder = 3}},
-            {new CatalogCards() {CatalogName = "美妝", CatalogOrder = 4}},
-            {new CatalogCards() {CatalogName = "學習", CatalogOrder = 5}},
-        };
+        private List<CardsViewModel> _cards = new List<CardsViewModel>();
 
         // GET
-        public IActionResult Show()
+        public IActionResult Index()
         {
-            ViewBag.list = _catalogCards;
-            return View();
-        }
-        
-        [Route("{controller}/show/{name}")]
-        public IActionResult ShowOneCard(string name)
-        {
-            if (_catalogCards.Any(x => x.CatalogName == name))
+            if (_cards.Count == 0)
             {
-                var order = _catalogCards.FindIndex(x => x.CatalogName == name);
-                ViewBag.card = _catalogCards[order];
-            }
-            else
-            {
-                ViewBag.card = _catalogCards[0];    
+                GenerateCards("未分類", 0);
             }
             
-            return View();
+            return View(_cards);
+        }
+
+        [Route("{Controller}/{name}")]
+        public IActionResult ShowOneCard(string name)
+        {
+            if (_cards.All(x => x.CatalogName != name))
+            {
+                var cardOrder = _cards.Count + 1;
+                GenerateCards(name, cardOrder);
+            }
+
+            var showCard = _cards[GetCardOrder(name)];
+            return View(showCard);
+        }
+
+        private void GenerateCards(string cardName, int cardOrder)
+        {
+            _cards.Add(new CardsViewModel() {CatalogName = cardName, CatalogOrder = cardOrder});
+        }
+
+        private int GetCardOrder(string name)
+        {
+            var order = _cards.FindIndex(x => x.CatalogName == name);
+            return order;
         }
     }
 }
---
+++ Expending-Card/Models/DetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Expending_Card.Models
+{
+    public class DetailsViewModel
+    {
+        public string ExpendingDate { get; set; }
+        public string ExpendingDetails { get; set; }
+        public int ExpendingPrice { get; set; }
+        public CardsViewModel ExpendingCard { get; set; }
+    }
+}
