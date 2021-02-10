469dfb12d71a76526c9c6c582ac7c0e3dc7c7628
litchi <d0509327@mail.fcu.edu.tw>
2021-02-11 01:31:55 +0800

    Create Detail View Model

---
+++ Expending-Card/ViewModels/DetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Expending_Card.Models
+{
+    public class DetailList
+    {
+        public string Detail { get; set; }
+        public DateTime Date { get; set; }
+        public int Price  { get; set; }
+        public Card Card  { get; set; }
+    }
+    
+    public class DetailViewModel
+    {
+        public List<DetailList> Details { get; set; }
+    }
+}
