d14f00dfb557a040b5b592ed36e8d6b75a2ecc00
litchi <d0509327@mail.fcu.edu.tw>
2021-02-11 02:13:47 +0800

    Details can be check in detail index page by expending view model

--- Expending-Card/ViewModels/DetailViewModel.cs
+++ Expending-Card/ViewModels/DetailViewModel.cs
@@ -6,7 +6,7 @@ namespace Expending_Card.Models
     public class DetailList
     {
         public string Detail { get; set; }
-        public DateTime Date { get; set; }
+        public string Date { get; set; }
         public int Price  { get; set; }
         public Card Card  { get; set; }
     }
@@ -14,5 +14,13 @@ namespace Expending_Card.Models
     public class DetailViewModel
     {
         public List<DetailList> Details { get; set; }
+
+        public void DefaultList()
+        {
+            Details = new List<DetailList>();
+            var card = new Card() {Name = "飲料", Order = 2};
+            var detail = new DetailList() {Detail = "冰淇淋奶茶", Card = card, Date = "2021/02/11", Price = 120};
+            Details.Add(detail);
+        }
     }
 }
